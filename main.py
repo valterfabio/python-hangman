a66b005d2edcfdf96bf4fe1756adbd3d3f377caa
Valter Fábio de Lira Torres <43069330+valterfabio@users.noreply.github.com>
2024-01-19 12:50:10 -0300

    improvements

--- main.py
+++ main.py
@@ -1,19 +1,60 @@
+import os
 import random
+from string import ascii_uppercase
+from printables import logo, stages
+from words import word_list
+
+def clean_console():
+    return os.system('cls')
 
 def print_solution(solution):
-    print(" ".join(solution))
+    print(" ".join(solution), end="\n\n")
 
-word_list = ['ardvark', 'baboon', 'camel']
-chosen_word = random.choice(word_list)
+clean_console()
 
+allowed_letters = set(ascii_uppercase)
+guessed_letters = set()
+
+chosen_word = random.choice(word_list).upper()
 solution = ["_" for _ in chosen_word]
-print_solution(solution)
+mistakes = 0
+has_won = False
 
-guess = input("Guess a letter: ").lower()
-print([guess==letter for letter in chosen_word])
+print(logo)
 
-for i, letter in enumerate(chosen_word):
-    if letter != "_" and letter == guess:
-        solution[i] = letter
+while mistakes < 6 and has_won is False:
 
-print_solution(solution)
+    print(stages[mistakes])
+    print_solution(solution)
+    guess = input("Guess a letter: ").upper()
+    clean_console()
+
+    if guess not in allowed_letters:
+        print(f"Please, choose only letters! The character {guess} is not allowed!")
+
+    elif guess in guessed_letters:
+        print(f"You have already chosen the letter {guess}. Chose another one!")
+
+    else:
+        guessed_letters.add(guess)
+        for i, letter in enumerate(chosen_word):
+            if letter == guess:
+                solution[i] = letter
+
+        if "_" not in solution:
+            has_won = True
+
+        if guess not in chosen_word:
+            print(f"\nThe letter {guess} is not in the word! You lost a life!")
+            mistakes += 1
+
+if has_won:
+    print(f"YOU WON! The word was {chosen_word}!\n")
+else:
+    print(stages[mistakes])
+    print(f"YOU LOST! The word was {chosen_word}!\n")
+
+
+
+
+
